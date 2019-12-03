2ae27173b8a03398c557773d723c25985b0de7ba
AndrewRedican <andrew.redican.mejia@gmail.com>
2019-12-03 13:01:48 +0000

    sort ui

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -17,7 +17,23 @@ import {
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'shards-react'
+
 class NavBar extends Component {
+  state = { dropdownOpen: false, sortAscending: true }
+
+  toggleDropdownOpen = () =>
+    this.setState({ dropdownOpen: !this.state.dropdownOpen })
+
+  setAscending = () => this.setState({ sortAscending: true })
+
+  setDescending = () => this.setState({ sortAscending: false })
+
   render() {
     return (
       <Navbar sticky='top' type='dark' theme='primary' expand='md'>
@@ -47,6 +63,29 @@ class NavBar extends Component {
               onChange={this.props.onInputChange}
             />
           </InputGroup>
+        </Nav>
+        <Nav navbar className='ml-auto'>
+          <Dropdown
+            inNavbar
+            open={this.state.dropdownOpen}
+            toggle={this.toggleDropdownOpen}
+          >
+            <DropdownToggle>Sort by Price</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem
+                active={!this.state.sortAscending}
+                onClick={this.setDescending}
+              >
+                Highest First
+              </DropdownItem>
+              <DropdownItem
+                active={this.state.sortAscending}
+                onClick={this.setAscending}
+              >
+                Lowest First
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </Nav>
         <Nav navbar className='ml-auto'>
           <NavLink active className='no-padding pointer'>
