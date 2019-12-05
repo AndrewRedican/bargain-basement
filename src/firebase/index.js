9d9ae38f3ff6cb1ed3c94b1692db93bbcadc3858
AndrewRedican <andrew.redican.mejia@gmail.com>
2019-12-05 12:24:33 +0000

    removed subscribe as not required/used in project

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -15,18 +15,3 @@ export function initializeFirebase(userAuthCallback) {
 /**
  * CRUD Access via RESTfull API Only
  */
-
-export async function subscribe(path, callBack) {
-  function enrichCallback(snapshot) {
-    const timestamp = new Date().getTime()
-    const data = snapshot.val()
-    const response = { data, timestamp, path }
-    callBack(response)
-  }
-  const listenerReference = db.ref(path).on('value', enrichCallback)
-  return listenerReference
-}
-
-export async function unsubscribe(path) {
-  db.ref(path).off()
-}
